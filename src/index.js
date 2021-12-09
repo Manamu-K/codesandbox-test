58cbf10a0979cc66fd8bae93af89f535cccc76c8
Manamu.K <64361362+Manamu-K@users.noreply.github.com>
2021-12-09 19:44:02 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-const val3 = "const変数";
-console.log(val3);
+// const val3 = "const変数";
+// console.log(val3);
 
-const name = "manamu";
-const age = 29;
-const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
-console.log(message1);
+// const name = "manamu";
+// const age = 29;
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+// console.log(message1);
 
-const message2 = `私の名前は${name}です。年齢は${age}です`;
-console.log(message2);
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
