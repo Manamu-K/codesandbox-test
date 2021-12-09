fc9361fd63ad02cb725ee64dad5e8c4c3745bf35
Manamu.K <64361362+Manamu-K@users.noreply.github.com>
2021-12-09 19:35:39 +0900

    テンプレートリテラル

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 const val3 = "const変数";
 console.log(val3);
+
+const name = "manamu";
+const age = 29;
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+console.log(message1);
+
+const message2 = `私の名前は${name}です。年齢は${age}です`;
+console.log(message2);
