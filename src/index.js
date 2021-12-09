b2110179eaeedeba3f47233047adac9eb5a8bbd4
Manamu.K <64361362+Manamu-K@users.noreply.github.com>
2021-12-09 19:56:15 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -9,18 +9,29 @@
 // const message2 = `私の名前は${name}です。年齢は${age}です`;
 // console.log(message2);
 
-function func1(str) {
-  return str;
-}
-console.log(func1("func1です"));
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+// const myProfile = {
+//   name: "manamu",
+//   age: 29
+// };
+// const message1 = `私の名前は${myProfile.name}です。年齢は${myProfile.age}です`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
