f9c8b68632a374cbe25ff8718a23199a3cf40ac4
Manamu.K <64361362+Manamu-K@users.noreply.github.com>
2021-12-12 22:29:31 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-// const val3 = "const変数";
-// console.log(val3);
+const val3 = "const変数";
+console.log(val3);
 
-// const name = "manamu";
-// const age = 29;
-// const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
-// console.log(message1);
+const name = "manamu";
+const age = 29;
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+console.log(message1);
 
-// const message2 = `私の名前は${name}です。年齢は${age}です`;
-// console.log(message2);
+const message2 = `私の名前は${name}です。年齢は${age}です`;
+console.log(message2);
 
-// function func1(str) {
-//   return str;
-// }
-// console.log(func1("func1です"));
+function func1(str) {
+  return str;
+}
+console.log(func1("func1です"));
 
-// // アロー関数
-// const func2 = (str) => {
-//   return str;
-// };
-// console.log(func2("func2です"));
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
 
-// const func3 = (num1, num2) => {
-//   return num1 + num2;
-// };
-// console.log(func3(10, 20));
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
 
-// const myProfile = {
-//   name: "manamu",
-//   age: 29
-// };
-// const message1 = `私の名前は${myProfile.name}です。年齢は${myProfile.age}です`;
-// console.log(message1);
+const myProfile = {
+  name: "manamu",
+  age: 29
+};
+const message1 = `私の名前は${myProfile.name}です。年齢は${myProfile.age}です`;
+console.log(message1);
 
-// const { name, age } = myProfile;
-// const message2 = `私の名前は${name}です。年齢は${age}です`;
-// console.log(message2);
+const { name, age } = myProfile;
+const message2 = `私の名前は${name}です。年齢は${age}です`;
+console.log(message2);
 
-// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
-// sayHello();
+const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+sayHello();
 
-// const arr1 = [1, 2];
-// console.log(arr1);
-// console.log(...arr1);
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// sumFunc(arr1[0], arr1[1]);
-// sumFunc(...arr1);
+const arr1 = [1, 2];
+console.log(arr1);
+console.log(...arr1);
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
 
-// const arr2 = [1, 2, 3, 4, 5];
-// const [num1, num2, ...arr3] = arr2;
-// console.log(num1);
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
 
-// function sum(x, y, z) {
-//   return x + y + z;
-// }
+function sum(x, y, z) {
+  return x + y + z;
+}
 
-// const numbers = [1, 2, 3];
-// const result = sum(...numbers);
+const numbers = [1, 2, 3];
+const result = sum(...numbers);
 
-// console.log(result);
+console.log(result);
